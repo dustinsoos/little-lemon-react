ffd18970bc129a3fc6b4c418307a58749939ccc7
Dustin Soos <dustinmsoos@gmail.com>
2023-06-10 02:42:37 -0400

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,13 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className=" container">
-      <div className="">
-        <nav className="">
-          <ul className=" collapse flex gap-5 font-sans md:visible">
-            <li>
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="">About</a>
-            </li>
-            <li>
-              <a href="">Menu</a>
-            </li>
-            <li>
-              <a href="">Reservations</a>
-            </li>
-            <li>
-              <a href="">Order Online</a>
-            </li>
-            <li>
-              <a href="">Login</a>
-            </li>
-          </ul>
+    <main className=" container mx-auto">
+      <div className="mt-6">
+        {/* <Image className="" src="/images/icons_assets/Logo.svg" width={300} height={300}></Image> */}
+        <nav className=" flex justify-between">
+          <Image src="/images/icons_assets/icon_hamburger-menu.svg" width={30} height={30} className=" cursor-pointer"></Image>
+          <Image src="/images/icons_assets/Basket.svg" width={30} height={30}></Image>
         </nav>
       </div>
     </main>
