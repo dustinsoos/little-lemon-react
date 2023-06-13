a544f742510c84bd9e4b92631190c6658ac51d66
Dustin Soos <dustinmsoos@gmail.com>
2023-06-13 18:50:40 -0400

    Setting up file structure

--- app/page.jsx
+++ app/page.jsx
@@ -5,21 +5,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className=" container mx-auto">
-      <div className="mt-6">
+    <main>
+      <div className="mt-6 container mx-auto">
         <nav className="">
           <div className=" flex justify-between">
-            <Image className="  hidden lg:block" src="/images/icons_assets/Logo.svg" width={300} height={300}></Image>
+            <Image className="  hidden lg:block" src="/images/Logo.svg" width={300} height={300}></Image>
             <button>
-              <Image src="/images/icons_assets/icon_hamburger-menu.svg" width={30} height={30} className=" cursor-pointer block lg:hidden"></Image>
+              <Image src="/images/icon_hamburger-menu.svg" width={30} height={30} className=" cursor-pointer block lg:hidden"></Image>
             </button>
             <button>
-              <Image src="/images/icons_assets/Basket.svg" width={30} height={30}></Image>
+              <Image src="/images/Basket.svg" width={30} height={30}></Image>
             </button>
           </div>
-          <ul id="menu" className=" grid collapse md:block">
+          <ul id="menu" className=" grid  md:block pt-5">
             <Link href="/home">Home</Link>
-            <Link href="/about">About</Link>
+            <Link href="/pages/about">About</Link>
             <Link href="/menu">Menu</Link>
             <Link href="/reservation">Reservation</Link>
             <Link href="/orderonline">Order Online</Link>
